4b17f638f44f12d7905c28b35873aff3cc381a10
Ishfaq-code <ishfaq.mohsin.main@gmail.com>
2026-09-17 16:40:53 -0400

    chore

--- api/register.php
+++ api/register.php
@@ -10,8 +10,10 @@ if($_SERVER['REQUEST_METHOD'] != 'POST'){
     response(405, "Method Not Allowed!", null);
 }
 
+// Get data from request
 $data = json_decode(file_get_contents("php://input"), true);
 
+// Validate data exists
 if (!is_array($data)) {
     http_response_code(400);
     response(400, "Request body must be valid JSON.", null);
@@ -23,17 +25,19 @@ $lastName  = trim($data["lastName"]);
 $login     = trim($data["login"]);
 $password  = $data["password"];
 
-
+// Validate data is non empty
 if ($firstName === "" || $lastName === "" || $login === "" || !is_string($password)){
     http_response_code(400);
     response(400, "Must fill in all required registration information", null);
 }
 
+// Validate password is strong
 if (!validatePassword($password)){
     http_response_code(400);
     response(400, "Password must contain at least 8 characters, a number and a special symbol!", null);
 }
 
+// Attempt to create a user in the database with credentials
 try{
     $db = getDB();
     if($db){
@@ -57,15 +61,16 @@ try{
 
 }
 catch(PDOException $e){
+    // Check if user exists error
     if (
         isset($e->errorInfo[1]) &&
         (int) $e->errorInfo[1] === 1062
     ) {
         http_response_code(409);
-        response(409, "That login already exists.", null);
+        response(409, "That login already exists.", $e);
     }
     http_response_code(500);
-    response(500, "Unable to create user.", null);
+    response(500, "Unable to create user.", $e);
 
 }
 
